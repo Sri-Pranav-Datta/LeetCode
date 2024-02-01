852ea2aa68b3d1d6aa68931ec8dd5b086f823cc3
CHAGANTI SRI PRANAV DATTA <114595832+Sri-Pranav-Datta@users.noreply.github.com>
2024-02-01 20:17:37 +0530

    Time: 2 ms (76.73%) | Memory: 7.2 MB (7.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
+        int a=0, b=1, c,i;
         if(n==0){
-            return 0;
+            return a;
         }
         if(n==1){
-            return 1;
+            return b;
         }
-            return fib(n-1)+ fib(n-2);
+        for(i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
     }
 };
