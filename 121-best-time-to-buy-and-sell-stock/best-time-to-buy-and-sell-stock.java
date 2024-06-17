7f51fc906aeff1ca92f3de8d20d143c3281ecab9
CHAGANTI SRI PRANAV DATTA <114595832+Sri-Pranav-Datta@users.noreply.github.com>
2024-06-17 22:28:10 +0530

    Time: 2 ms (72.30%) | Memory: 62 MB (13.12%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int n = prices.length;
+        int maxProfit=0;
+
+        for(int i=1; i<n; i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            else{
+                if(prices[i]-min > maxProfit){
+                    maxProfit=prices[i]-min;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+    
+}
