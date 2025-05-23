9a131550289232255d3b7cf2599e5513cc9eb468
C PRANAV DATTA <114595832+Sri-Pranav-Datta@users.noreply.github.com>
2025-05-23 16:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (36.37%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,21 +14,26 @@
  * }
  */
 class Solution {
+
+    void prelev(TreeNode root, int l, List<List<Integer>> res) {
+        if(root == null) {
+            return;
+        }
+        if(res.size() == l) {
+            List<Integer> sub = new ArrayList<Integer>();
+            sub.add(root.val);
+            res.add(sub);
+        }
+        else {
+            res.get(l).add(root.val);
+        }
+        prelev(root.left, l+1, res);
+        prelev(root.right, l+1, res);
+    } 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
         List<List<Integer>> res = new ArrayList<>();
-        if(root == null) return res;
-        queue.offer(root);
-        while(!queue.isEmpty()) {
-            int n = queue.size();
-            List<Integer> sublist = new ArrayList<Integer>();
-            for(int i = 0; i < n; i++) {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                sublist.add(queue.poll().val);
-            } 
-            res.add(sublist);
-        } 
+        prelev(root, 0, res);
         return res;
     }
+
 }
