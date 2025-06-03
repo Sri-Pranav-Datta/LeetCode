441a29c3fef81dd92be0ae15ce8985dfebac5cbb
C PRANAV DATTA <114595832+Sri-Pranav-Datta@users.noreply.github.com>
2025-06-03 12:58:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (85.32%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode res = root;
+        while(res != null) {
+            if(val > res.val) res = res.right;
+            else if(val < res.val) res = res.left;
+            else break;
+        }
+        return res;
+    }
+}
