eb7c512aa0a67a4d078dc1989b1eb0b9c8b4fc95
C PRANAV DATTA <114595832+Sri-Pranav-Datta@users.noreply.github.com>
2025-06-10 18:11:08 +0530

    Time: 2 ms (68.28%) | Memory: 43.1 MB (12.72%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -9,37 +9,27 @@ class Pair {
 }
  
 class Solution {
-    boolean finished(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean res = true;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j]==1){
-                    res = false;
-                }
-            }
-        }
-        return res;
-    }
+    
     public int orangesRotting(int[][] grid) {
         Queue<Pair> q = new LinkedList<Pair>();
         int n = grid.length;
         int m = grid[0].length;
+        int fresh = 0;
         for(int i = 0; i <n; i++) {
             for(int j = 0; j <m; j++) {
                 if(grid[i][j]==2) {
                     //add initial rotten oranges
                     q.offer(new Pair(i,j));
                 }
+                if(grid[i][j]==1) {
+                    fresh++;
+                }
             }
         }
         int time = 0;
         Pair[] directions = {new Pair(-1,0), new Pair(1,0), new Pair(0, -1), new Pair(0, 1)};
         while(!q.isEmpty()) {
-            if(finished(grid)) {
-                break;
-            }
+            if(fresh == 0) break;
             int s = q.size();
             time++;
             for(int i = 0; i < s; i++) {
@@ -51,12 +41,13 @@ class Solution {
                     int ncol = curcol + d.c;
                     if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1) {
                         grid[nrow][ncol] = 2;
+                        fresh--;
                         q.offer(new Pair(nrow, ncol));
                     }
                 }
             }
             
         }
-        return finished(grid) ? time : -1;
+        return (fresh == 0) ? time : -1;
     }
 }
