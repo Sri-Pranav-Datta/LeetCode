96fdbe8b14e85e794028011463906a8677ebe283
C PRANAV DATTA <114595832+Sri-Pranav-Datta@users.noreply.github.com>
2025-09-18 22:35:18 +0530

    Time: 932 ms (68.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select r.contest_id, round( count(r.user_id) * 100 / (select count(*) from users) ,2) as percentage
+from users u
+join register r
+on r.user_id = u.user_id
+group by r.contest_id
+order by percentage desc, r.contest_id asc
